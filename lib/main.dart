902b456fc1e060b986febb734862f8b90268f713
mj <onyangomaureen95@gmail.com>
2019-02-16 14:03:19 +0300

    create hello rectangle class with a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,3 +15,19 @@ runApp(
 );
 }
 
+class HelloRectangle extends StatelessWidget{
+  @override
+  Widget build(context){
+    return Center(
+    child: Container(
+      color: Colors.amberAccent,
+      height: 400.0,
+      width: 300.0,
+      child: Center(
+        child: Text('Hello there', style: TextStyle(fontSize: 50.0), textAlign: TextAlign.center,
+        ),
+      ),
+    ),
+    );
+  }
+}
