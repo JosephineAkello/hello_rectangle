c9e5d9dea5f9c05242207821b87e6545209e20b1
mj <onyangomaureen95@gmail.com>
2019-02-16 15:31:39 +0300

    add padding and and formating

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Hello Rectangle',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hello Rectangale'),
+const  _padding= EdgeInsets.all(16.0);
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Rectangle',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangale'),
+        ),
+        body: HelloRectangle(),
       ),
-      body: HelloRectangle(),
     ),
-  ),
-);
+  );
 }
 
-class HelloRectangle extends StatelessWidget{
+class HelloRectangle extends StatelessWidget {
   @override
-  Widget build(context){
+  Widget build(context) {
     return Center(
-    child: Container(
-      color: Colors.amberAccent,
-      height: 400.0,
-      width: 300.0,
-      child: Center(
-        child: Text('Hello there', style: TextStyle(fontSize: 50.0), textAlign: TextAlign.center,
+      child: Container(
+        padding:_padding,
+        color: Colors.amberAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello there',
+            style: TextStyle(fontSize: 50.0),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
-    ),
     );
   }
 }
